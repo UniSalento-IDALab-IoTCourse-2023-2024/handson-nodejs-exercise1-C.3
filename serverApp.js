a5002f8927a26aad782d5a8ba5684f2a7fc46949
Teodoro Montanaro <teodoro.montanaro@unisalento.it>
2024-04-16 18:35:16 +0200

    working version

--- serverApp.js
+++ serverApp.js
@@ -3,6 +3,7 @@ var app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
 app.post("/temperature", (req, res, next) => {
     console.log("receivedData");
     res.sendStatus(200);
